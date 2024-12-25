08ef22790fa0b996550310a7226c46e6144e92a3
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2024-12-26 05:06:39 +0700

    feat(user-service): implement health check REST API

--- user-service/internal/driver/http/health_check/health_check_rest.go
+++ user-service/internal/driver/http/health_check/health_check_rest.go
@@ -4,11 +4,25 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/harmonify/movie-reservation-system/user-service/lib/http/response"
 	"github.com/harmonify/movie-reservation-system/user-service/lib/tracer"
+	"go.uber.org/fx"
 )
 
 type HealthCheckRestHandler interface {
 	Register(g *gin.RouterGroup)
 	HealthCheck(c *gin.Context)
+}
+
+type HealthCheckRestHandlerParam struct {
+	fx.In
+
+	Response response.HttpResponse
+	Tracer   tracer.Tracer
+}
+
+type HealthCheckRestHandlerResult struct {
+	fx.Out
+
+	HealthCheckRestHandler HealthCheckRestHandler
 }
 
 type healthCheckRestHandlerImpl struct {
@@ -20,10 +34,12 @@ type HealthCheckResponse struct {
 	Ok bool `json:"ok"`
 }
 
-func NewHealthCheckRestHandler(response response.HttpResponse, tracer tracer.Tracer) HealthCheckRestHandler {
-	return &healthCheckRestHandlerImpl{
-		response: response,
-		tracer:   tracer,
+func NewHealthCheckRestHandler(p HealthCheckRestHandlerParam) HealthCheckRestHandlerResult {
+	return HealthCheckRestHandlerResult{
+		HealthCheckRestHandler: &healthCheckRestHandlerImpl{
+			response: p.Response,
+			tracer:   p.Tracer,
+		},
 	}
 }
 
@@ -40,7 +56,7 @@ func (h *healthCheckRestHandlerImpl) HealthCheck(c *gin.Context) {
 		}
 	)
 
-	_, span := h.tracer.Start(ctx, "healthCheckRestHandlerImpl.HealthCheck")
+	_, span := h.tracer.StartSpanWithCaller(ctx)
 	defer span.End()
 
 	h.response.Send(c, data, err)
