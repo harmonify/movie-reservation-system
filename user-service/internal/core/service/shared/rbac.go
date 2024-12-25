cdeafca9e652eece03138e5aa8381bd2dd4b45b8
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2024-12-26 05:04:27 +0700

    feat(user-service): add storage interfaces

---
+++ user-service/internal/core/service/shared/rbac.go
@@ -0,0 +1,57 @@
+package shared_service
+
+import (
+	"context"
+
+	"github.com/harmonify/movie-reservation-system/user-service/lib/database"
+)
+
+var (
+	ActionGet    Action = "GET"
+	ActionPost   Action = "POST"
+	ActionPut    Action = "PUT"
+	ActionDelete Action = "DELETE"
+
+	RoleUser  Role = "user"
+	RoleAdmin Role = "admin"
+)
+
+type (
+	Action string
+	Role   string
+
+	RbacStorage interface {
+		WithTx(tx *database.Transaction) RbacStorage
+		CheckPermission(ctx context.Context, p CheckPermissionParam) (bool, error)
+		GrantPermission(ctx context.Context, p GrantPermissionParam) (bool, error)
+		BulkGrantPermission(ctx context.Context, p []GrantPermissionParam) (bool, error)
+		RevokePermission(ctx context.Context, p RevokePermissionParam) (bool, error)
+		GrantRole(ctx context.Context, p GrantRoleParam) (bool, error)
+	}
+
+	CheckPermissionParam struct {
+		UUID     string
+		Domain   string
+		Resource string
+		Action   Action
+	}
+
+	GrantPermissionParam struct {
+		UUID     string
+		Domain   string
+		Resource string
+		Action   Action
+	}
+
+	RevokePermissionParam struct {
+		UUID     string
+		Domain   string
+		Resource string
+		Action   Action
+	}
+
+	GrantRoleParam struct {
+		UUID string
+		Role Role
+	}
+)
---
+++ user-service/internal/core/service/shared/storage.go
@@ -0,0 +1,46 @@
+package shared_service
+
+import (
+	"context"
+
+	"github.com/harmonify/movie-reservation-system/user-service/internal/core/entity"
+	"github.com/harmonify/movie-reservation-system/user-service/lib/database"
+)
+
+type (
+	Storage interface {
+		Transaction(fc func(tx *database.Transaction) error) error
+	}
+
+	UserStorage interface {
+		WithTx(tx *database.Transaction) UserStorage
+		FindUser(ctx context.Context, findModel entity.FindUser) (*entity.User, error)
+		SaveUser(ctx context.Context, createModel entity.SaveUser) (*entity.User, error)
+		UpdateUser(ctx context.Context, userUUID string, updateModel entity.UpdateUser) (*entity.User, error)
+		SoftDeleteUser(ctx context.Context, userUUID string) error
+	}
+
+	UserSessionStorage interface {
+		WithTx(tx *database.Transaction) UserSessionStorage
+		FindSession(ctx context.Context, findModel entity.FindUserSession) (*entity.UserSession, error)
+		SaveSession(ctx context.Context, createModel entity.SaveUserSession) (*entity.UserSession, error)
+		RevokeSession(ctx context.Context, refreshToken string) (err error)
+		RevokeManySession(ctx context.Context, refreshTokens []string) (err error)
+	}
+
+	UserKeyStorage interface {
+		WithTx(tx *database.Transaction) UserKeyStorage
+		FindUserKey(ctx context.Context, findModel entity.FindUserKey) (*entity.UserKey, error)
+		SaveUserKey(ctx context.Context, createModel entity.SaveUserKey) (*entity.UserKey, error)
+		UpdateUserKey(ctx context.Context, userUUID string, updateModel entity.UpdateUserKey) (*entity.UserKey, error)
+	}
+
+	OtpStorage interface {
+		SaveEmailVerificationToken(ctx context.Context, p SaveEmailVerificationTokenParam) error
+		GetEmailVerificationToken(ctx context.Context, email string) (string, error)
+		SavePhoneOtp(ctx context.Context, p SavePhoneOtpParam) error
+		GetPhoneOtp(ctx context.Context, phoneNumber string) (string, error)
+		IncrementPhoneOtpAttempt(ctx context.Context, phoneNumber string) error
+		GetPhoneOtpAttempt(ctx context.Context, phoneNumber string) (int, error)
+	}
+)
